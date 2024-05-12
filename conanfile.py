b39c459a1e714d65a9c672c780af4a1b18297745
JungerBoyo <pZ010001011111@proton.me>
2024-05-12 14:43:00 +0200

    lab9 completed

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,9 @@ class ChangeMeRecipe(ConanFile):
         #   self.options["glad"].gl_version = "4.5"
 
     def requirements(self):
-        self.requires("spdlog/1.13.0")        
+        self.requires("pistache/cci.20240107")
+        self.requires("spdlog/1.13.0")
+        self.requires("nlohmann_json/3.11.3")
         # self.requires("glad/0.1.36")
         # conditional requires (it happens too often)
         # if (self.settings.os != 'Windows'): 
